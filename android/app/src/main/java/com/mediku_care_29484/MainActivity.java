4cc5790bb787be11dd560d4e40278e556a126867
Crowdbotics <team@crowdbotics.com>
2021-08-05 00:41:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/mediku_care_29484/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mediku_care_29484;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "mediku_care_29484";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mediku_care_29484Identifier",
+  url: "https://mediku_care_29484Identifier.botics.co",
+  api: "https://mediku_care_29484Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
